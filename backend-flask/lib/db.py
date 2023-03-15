ad2302f0d42257e62662d1ce4258b9e61b0fc0d0
ibnomarmahmoud <mahmoudoosman007@gmail.com>
2023-03-15 08:53:20 +0000

    Home Activities fetched from Postgres/RDS

---
+++ backend-flask/lib/db.py
@@ -0,0 +1,25 @@
+'''
+The idea of the Pool is that we have limited connections to the DB and we want the clients to re-use the opened connections , for which pools are used
+'''
+
+from psycopg_pool import ConnectionPool
+import os
+
+def query_wrap_object(template):
+  sql = f"""
+  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+  {template}
+  ) object_row);
+  """
+  return sql
+
+def query_wrap_array(template):
+  sql = f"""
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+  {template}
+  ) array_row);
+  """
+  return sql
+
+connection_url = os.getenv("CONNECTION_URL")
+pool = ConnectionPool(connection_url)
