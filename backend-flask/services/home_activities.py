ebc084c599de4310e68fc7bfcb1b5c9037652406
ibnomarmahmoud <mahmoudoosman007@gmail.com>
2023-03-17 18:53:28 +0000

    Create Activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,41 +1,17 @@
 from datetime import datetime, timedelta, timezone
-from lib.db import pool, query_wrap_array
 from opentelemetry import trace
-tracer = trace.get_tracer("new_trace")
 
-import watchtower
-import logging
-from time import strftime
+from lib.db import db
+
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(LOGGER):
-    LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
-    with tracer.start_as_current_span("Home-Activities-span"):
-      span = trace.get_current_span()
-      span.set_attribute("app.endpoint", "Home_Activities")
-      now = datetime.now(timezone.utc).astimezone()
-      sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
-      print(sql)
-      with tracer.start_as_current_span("Home-Activities-span-Inner") as inner_span:
-        with pool.connection() as conn:
-          with conn.cursor() as cur:
-            cur.execute(sql)
-            json = cur.fetchone()
-        return json[0]
-
-
+  def run(cognito_user_id=None):
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
