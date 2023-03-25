f44f17ecc53f65a05a89dfc47b1ccfe022fae550
ibnomarmahmoud <mahmoudoosman007@gmail.com>
2023-03-25 07:28:14 +0000

    Update Cognito User ID

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Mahmoud Omar', 'MahmoudOmar' ,'MOCK'),
+  ('Ibarhim Omar', 'ibnomar6' ,'MOCK'),
+  ('Ibrahim Mahmoud', 'Hema' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'MahmoudOmar' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
